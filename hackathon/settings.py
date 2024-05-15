d98db236bb040e4b767e854034c701c2d55032a2
Rohit Jadhav <99168154+rjcoder86@users.noreply.github.com>
2024-05-15 11:20:30 +0530

    fix : smtp setting changed

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -170,9 +170,9 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 # EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_USER = 'ranjitshinde9404@gmail.com'
+EMAIL_HOST_USER = 'YOUR GMAIL'
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_HOST_PASSWORD = 'ranjit9404'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
 EMAIL_USE_TLS = True
 
 
